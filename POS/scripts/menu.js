2a9a45a21b38260755f35de0a5aeaa99211c440e
Akshay <akshayghatge6011@gmail.com>
2025-08-27 07:51:28 -0400

    Added images and added hamburger menu + js file

---
+++ POS/scripts/menu.js
@@ -0,0 +1,9 @@
+//variables
+
+//Hamburger menu
+const toggleButton = document.getElementById('menu-toggle');
+const navMenu = document.querySelector('.nav-menu');
+
+  toggleButton.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+  });
