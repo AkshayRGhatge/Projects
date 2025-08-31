6e1ba68de0f910a3e26b1addab99f51e2360f34d
Akshay <akshayghatge6011@gmail.com>
2025-08-31 18:28:47 -0400

    adjusted the layout of the pos

--- POS/scripts/menu.js
+++ POS/scripts/menu.js
@@ -1,9 +1,19 @@
 //variables
-
-//Hamburger menu
 const toggleButton = document.getElementById('menu-toggle');
-const navMenu = document.querySelector('.nav-menu');
+const navMenu = document.getElementById('sidebar');
+const mainContent=document.getElementsByClassName('main-content');
 
-  toggleButton.addEventListener('click', () => {
-    navMenu.classList.toggle('active');
-  });
+//If hamburger got click make the menu display
+toggleButton.addEventListener('click', () => {
+    navMenu.classList.toggle('display-none');
+
+    //if the menu contain displaynone from left make the main content 0px other wise 150px
+    if(navMenu.classList.contains('display-none'))
+    {
+      mainContent[0].style.left='0';
+    }
+    else{
+       mainContent[0].style.left='150px';
+    }
+});
+
