a9143843e9337bd9343e22ae5b23f39270546073
Akshay <akshayghatge6011@gmail.com>
2025-09-04 07:42:40 -0400

    Added drink and starter.js files

---
+++ POS/data/drinks.js
@@ -0,0 +1,268 @@
+export const Drinks = [
+  {
+    id: "f43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    image: "img/drinks/blue-moon-can.jpg",
+    name: "Blue Moon Can",
+    priceCents: 925,
+    keywords: [
+      "beer",
+      "blue moon",
+      "wheat beer",
+      "canned",
+      "alcoholic"
+    ],
+    category:'Drinks'
+  },
+  {
+    id: "f43638ce-327a-4ec4-896f-486349e85a3d",
+    image: "img/drinks/canadian-molson-can.jpg",
+    name: "Canadian Molson Can",
+    priceCents: 850,
+    keywords: [
+      "beer",
+      "molson",
+      "canadian",
+      "lager",
+      "alcoholic"
+    ],
+    category:'Drinks'
+  },
+  {
+    id: "f43638ce-0f35-48f5-b7a3-1ea210004f2e",
+    image: "img/drinks/chai.jpg",
+    name: "Masala Chai",
+    priceCents: 675,
+    keywords: [
+      "tea",
+      "chai",
+      "spiced",
+      "indian",
+      "hot beverage"
+    ],
+    category:'Drinks'
+  },
+  {
+    id: "f43638ce-8f36-462b-b68a-8182611d9add",
+    image: "img/drinks/cocolada.jpg",
+    name: "Cocolada",
+    priceCents: 795,
+    keywords: [
+      "coconut",
+      "tropical",
+      "cocktail",
+      "creamy",
+      "refreshing"
+    ],
+    category:'Drinks'
+  },
+  {
+    id: "f43638ce-64d2-4137-8860-1f5a963e534b",
+    image: "img/drinks/coke.jpg",
+    name: "Coca Cola",
+    priceCents: 620,
+    keywords: [
+      "cola",
+      "soda",
+      "carbonated",
+      "classic",
+      "refreshing"
+    ],
+    category:'Drinks'
+  },
+  {
+    id: "f43638ce-5a19-4bcb-a5d1-158a74287c53",
+    image: "img/drinks/corona-bottle.jpg",
+    name: "Corona Bottle",
+    priceCents: 980,
+    keywords: [
+      "beer",
+      "corona",
+      "mexican",
+      "light beer",
+      "bottle"
+    ],
+    category:'Drinks',
+  },
+  {
+    id: "f43638ce-a18b-4e2a-9250-31e67412f98d",
+    image: "img/drinks/diet-coke-can.jpg",
+    name: "Diet Coke Can",
+    priceCents: 575,
+    keywords: [
+      "diet cola",
+      "zero calories",
+      "carbonated",
+      "canned",
+      "sugar free"
+    ],
+    category:'Drinks',
+  },
+  {
+    id: "f43638ce-0e56-475b-adde-4f24dfed3a04",
+    image: "img/drinks/kingfisher-bottle.jpg",
+    name: "Kingfisher Bottle",
+    priceCents: 895,
+    keywords: [
+      "beer",
+      "kingfisher",
+      "indian",
+      "lager",
+      "premium"
+    ],
+    category:'Drinks',
+  },
+  {
+    id: "f43638ce-9a15-4979-b459-585b0d0545b9",
+    image: "img/drinks/lemonade.jpg",
+    name: "Fresh Lemonade",
+    priceCents: 650,
+    keywords: [
+      "lemonade",
+      "citrus",
+      "fresh",
+      "tangy",
+      "summer drink"
+    ],
+    category:'Drinks',
+  },
+  {
+    id: "f43638ce-e98c-42aa-8c55-b6b79996769a",
+    image: "img/drinks/mango-lassi.jpg",
+    name: "Mango Lassi",
+    priceCents: 745,
+    keywords: [
+      "lassi",
+      "mango",
+      "yogurt drink",
+      "indian",
+      "creamy"
+    ],
+    category:'Drinks',
+  },
+  {
+    id: "f43638ce-4d27-4872-89f6-5bcb052746d7",
+    image: "img/drinks/masala-pomegranate.jpg",
+    name: "Masala Pomegranate",
+    priceCents: 720,
+    keywords: [
+      "pomegranate",
+      "masala",
+      "spiced",
+      "antioxidant",
+      "healthy"
+    ],
+    category:'Drinks',
+  },
+  {
+    id: "f43638ce-ea98-41ee-9285-b916638cac4a",
+    image: "img/drinks/masala-soda-and-masala-shikanji.jpg",
+    name: "Masala Soda & Shikanji",
+    priceCents: 690,
+    keywords: [
+      "masala soda",
+      "shikanji",
+      "spiced",
+      "street drink",
+      "refreshing"
+    ],
+    category:'Drinks',
+  },
+  {
+    id: "f43638ce-bc79-49c6-bc11-1af7e3651358",
+    image: "img/drinks/pepsi-can.jpg",
+    name: "Pepsi Can",
+    priceCents: 595,
+    keywords: [
+      "cola",
+      "pepsi",
+      "carbonated",
+      "canned",
+      "classic"
+    ],
+    category:'Drinks',
+  },
+  {
+    id: "f43638ce-386d-432e-82f0-6fb1ee7bf969",
+    image: "img/drinks/salted-lassi.jpg",
+    name: "Salted Lassi",
+    priceCents: 725,
+    keywords: [
+      "lassi",
+      "salted",
+      "yogurt drink",
+      "savory",
+      "traditional"
+    ],
+    category:'Drinks',
+  },
+  {
+    id: "f43638ce-ebc9-476a-989c-c78a40ee5cd9",
+    image: "img/drinks/sparkling-grapes.jpg",
+    name: "Sparkling Grapes",
+    priceCents: 775,
+    keywords: [
+      "grape juice",
+      "sparkling",
+      "fizzy",
+      "fruity",
+      "non alcoholic"
+    ],
+    category:'Drinks',
+  },
+  {
+    id: "f43638ce-aff4-435f-9975-517cfaba2ece",
+    image: "img/drinks/spicy-herbaceous-margarita.jpg",
+    name: "Spicy Herbaceous Margarita",
+    priceCents: 995,
+    keywords: [
+      "margarita",
+      "spicy",
+      "herbs",
+      "cocktail",
+      "premium"
+    ],
+    category:'Drinks',
+  },
+  {
+    id: "f43638ce-f540-454e-8a1e-363f25dbae7d",
+    image: "img/drinks/spring-water.jpg",
+    name: "Spring Water",
+    priceCents: 525,
+    keywords: [
+      "water",
+      "spring water",
+      "pure",
+      "natural",
+      "hydrating"
+    ],
+    category:'Drinks',
+  },
+  {
+    id: "f43638ce-3067-4e68-a5ba-d827ac0be010",
+    image: "img/drinks/sprite.jpg",
+    name: "Sprite",
+    priceCents: 610,
+    keywords: [
+      "lemon lime",
+      "sprite",
+      "carbonated",
+      "clear",
+      "citrus"
+    ],
+    category:'Drinks',
+  },
+  {
+    id: "f43638ce-1377-42bc-89a5-e572d19252e2",
+    image: "img/drinks/virgin-mojito.jpg",
+    name: "Virgin Mojito",
+    priceCents: 765,
+    keywords: [
+      "mojito",
+      "mint",
+      "lime",
+      "virgin",
+      "refreshing"
+    ],
+    category:'Drinks',
+  }
+]
---
+++ POS/data/starter.js
@@ -0,0 +1,436 @@
+export const Starter = [
+  {
+    id: "g43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    image: "img/starter/air-fryer-falafel.jpg",
+    name: "Air Fryer Falafel",
+    priceCents: 950,
+    keywords: [
+      "falafel",
+      "air fryer",
+      "chickpea",
+      "healthy",
+      "mediterranean"
+    ],
+    category:'Starter'
+  },
+  {
+    id: "g43638ce-327a-4ec4-896f-486349e85a3d",
+    image: "img/starter/aloo-chaat.jpg",
+    name: "Aloo Chaat",
+    priceCents: 775,
+    keywords: [
+      "potato",
+      "chaat",
+      "street food",
+      "tangy",
+      "spicy"
+    ],
+    category:'Starter'
+  },
+  {
+    id: "g43638ce-0f35-48f5-b7a3-1ea210004f2e",
+    image: "img/starter/aloo-pakora.jpg",
+    name: "Aloo Pakora",
+    priceCents: 825,
+    keywords: [
+      "potato",
+      "pakora",
+      "fritters",
+      "crispy",
+      "monsoon snack"
+    ],
+    category:'Starter'
+  },
+  {
+    id: "g43638ce-8f36-462b-b68a-8182611d9add",
+    image: "img/starter/bajri-vada.jpg",
+    name: "Bajri Vada",
+    priceCents: 890,
+    keywords: [
+      "bajra",
+      "vada",
+      "millet",
+      "gujarati",
+      "healthy"
+    ],
+    category:'Starter'
+  },
+  {
+    id: "g43638ce-64d2-4137-8860-1f5a963e534b",
+    image: "img/starter/bang-bang-cauliflower.jpg",
+    name: "Bang Bang Cauliflower",
+    priceCents: 1075,
+    keywords: [
+      "cauliflower",
+      "bang bang",
+      "spicy",
+      "fusion",
+      "crispy"
+    ],
+    category:'Starter'
+  },
+  {
+    id: "g43638ce-5a19-4bcb-a5d1-158a74287c53",
+    image: "img/starter/batata-vada.jpg",
+    name: "Batata Vada",
+    priceCents: 720,
+    keywords: [
+      "potato",
+      "vada",
+      "maharashtrian",
+      "fried",
+      "street food"
+    ],
+    category:'Starter',
+  },
+  {
+    id: "g43638ce-a18b-4e2a-9250-31e67412f98d",
+    image: "img/starter/bread-dipping-oil.jpg",
+    name: "Bread Dipping Oil",
+    priceCents: 675,
+    keywords: [
+      "bread",
+      "dipping oil",
+      "herbs",
+      "appetizer",
+      "italian"
+    ],
+    category:'Starter',
+  },
+  {
+    id: "g43638ce-0e56-475b-adde-4f24dfed3a04",
+    image: "img/starter/chickpea-chaat.jpg",
+    name: "Chickpea Chaat",
+    priceCents: 800,
+    keywords: [
+      "chickpea",
+      "chaat",
+      "healthy",
+      "protein",
+      "tangy"
+    ],
+    category:'Starter',
+  },
+  {
+    id: "g43638ce-9a15-4979-b459-585b0d0545b9",
+    image: "img/starter/chipotle-queso-blanco.jpg",
+    name: "Chipotle Queso Blanco",
+    priceCents: 985,
+    keywords: [
+      "cheese dip",
+      "chipotle",
+      "queso",
+      "mexican",
+      "spicy"
+    ],
+    category:'Starter',
+  },
+  {
+    id: "g43638ce-e98c-42aa-8c55-b6b79996769a",
+    image: "img/starter/corn-chaat-recipe.jpg",
+    name: "Corn Chaat",
+    priceCents: 750,
+    keywords: [
+      "corn",
+      "chaat",
+      "street food",
+      "sweet corn",
+      "tangy"
+    ],
+    category:'Starter',
+  },
+  {
+    id: "g43638ce-4d27-4872-89f6-5bcb052746d7",
+    image: "img/starter/creamy-tomato-soup.jpg",
+    name: "Creamy Tomato Soup",
+    priceCents: 855,
+    keywords: [
+      "tomato soup",
+      "creamy",
+      "comfort food",
+      "warm",
+      "classic"
+    ],
+    category:'Starter',
+  },
+  {
+    id: "g43638ce-ea98-41ee-9285-b916638cac4a",
+    image: "img/starter/dahi-vada.jpg",
+    name: "Dahi Vada",
+    priceCents: 795,
+    keywords: [
+      "yogurt",
+      "vada",
+      "cooling",
+      "tangy",
+      "traditional"
+    ],
+    category:'Starter',
+  },
+  {
+    id: "g43638ce-bc79-49c6-bc11-1af7e3651358",
+    image: "img/starter/dill-leaves-pakoda.jpg",
+    name: "Dill Leaves Pakoda",
+    priceCents: 870,
+    keywords: [
+      "dill leaves",
+      "pakoda",
+      "herbs",
+      "crispy",
+      "aromatic"
+    ],
+    category:'Starter',
+  },
+  {
+    id: "g43638ce-386d-432e-82f0-6fb1ee7bf969",
+    image: "img/starter/instant-pot-hummus.jpg",
+    name: "Instant Pot Hummus",
+    priceCents: 925,
+    keywords: [
+      "hummus",
+      "instant pot",
+      "chickpea",
+      "healthy",
+      "mediterranean"
+    ],
+    category:'Starter',
+  },
+  {
+    id: "g43638ce-ebc9-476a-989c-c78a40ee5cd9",
+    image: "img/starter/khaman-dhokla-recipe.jpg",
+    name: "Khaman Dhokla",
+    priceCents: 815,
+    keywords: [
+      "dhokla",
+      "gujarati",
+      "steamed",
+      "healthy",
+      "spongy"
+    ],
+    category:'Starter',
+  },
+  {
+    id: "g43638ce-aff4-435f-9975-517cfaba2ece",
+    image: "img/starter/masala-papad.jpg",
+    name: "Masala Papad",
+    priceCents: 650,
+    keywords: [
+      "papad",
+      "masala",
+      "crispy",
+      "topping",
+      "appetizer"
+    ],
+    category:'Starter',
+  },
+  {
+    id: "g43638ce-f540-454e-8a1e-363f25dbae7d",
+    image: "img/starter/methi-muthia.jpg",
+    name: "Methi Muthia",
+    priceCents: 780,
+    keywords: [
+      "fenugreek",
+      "muthia",
+      "steamed",
+      "gujarati",
+      "healthy"
+    ],
+    category:'Starter',
+  },
+  {
+    id: "g43638ce-3067-4e68-a5ba-d827ac0be010",
+    image: "img/starter/mini-paneer-samosa.jpg",
+    name: "Mini Paneer Samosa",
+    priceCents: 1025,
+    keywords: [
+      "samosa",
+      "paneer",
+      "mini",
+      "fried",
+      "crispy"
+    ],
+    category:'Starter',
+  },
+  {
+    id: "g43638ce-1377-42bc-89a5-e572d19252e2",
+    image: "img/starter/onion-salad.jpg",
+    name: "Onion Salad",
+    priceCents: 625,
+    keywords: [
+      "onion",
+      "salad",
+      "fresh",
+      "raw",
+      "healthy"
+    ],
+    category:'Starter',
+  },
+  {
+    id: "g43638ce-8b40-4ca5-9142-b61fe3d98c85",
+    image: "img/starter/paneer-pakora.jpg",
+    name: "Paneer Pakora",
+    priceCents: 995,
+    keywords: [
+      "paneer",
+      "pakora",
+      "cottage cheese",
+      "fried",
+      "crispy"
+    ],
+    category:'Starter',
+  },
+  {
+    id: "g43638ce-79ef-4cf3-a6cf-6dbe532a1b4a",
+    image: "img/starter/paneer-tikka.jpg",
+    name: "Paneer Tikka",
+    priceCents: 1095,
+    keywords: [
+      "paneer",
+      "tikka",
+      "grilled",
+      "marinated",
+      "popular"
+    ],
+    category:'Starter',
+  },
+  {
+    id: "g43638ce-4e72-45cc-935c-9e13d79cc57f",
+    image: "img/starter/pani-puri.jpg",
+    name: "Pani Puri",
+    priceCents: 745,
+    keywords: [
+      "pani puri",
+      "golgappa",
+      "street food",
+      "tangy water",
+      "crispy"
+    ],
+    category:'Starter',
+  },
+  {
+    id: "g43638ce-6055-422a-a666-b34ba28b76d4",
+    image: "img/starter/papdi-chaat.jpg",
+    name: "Papdi Chaat",
+    priceCents: 770,
+    keywords: [
+      "papdi",
+      "chaat",
+      "crispy",
+      "toppings",
+      "street food"
+    ],
+    category:'Starter',
+  },
+  {
+    id: "g43638ce-3501-4b17-9889-a3bad6fb585f",
+    image: "img/starter/parmesan-crusted-potatoes.jpg",
+    name: "Parmesan Crusted Potatoes",
+    priceCents: 940,
+    keywords: [
+      "potato",
+      "parmesan",
+      "crusted",
+      "baked",
+      "cheesy"
+    ],
+    category:'Starter',
+  },
+  {
+    id: "g43638ce-3501-4b17-9889-a3bad6fb5877",
+    image: "img/starter/rava-dhokla.jpg",
+    name: "Rava Dhokla",
+    priceCents: 835,
+    keywords: [
+      "rava",
+      "dhokla",
+      "semolina",
+      "steamed",
+      "gujarati"
+    ],
+    category:'Starter',
+  },
+  {
+    id: "g43638ce-3501-4b17-9889-a3bad6fb5878",
+    image: "img/starter/sabudana-tikki.jpg",
+    name: "Sabudana Tikki",
+    priceCents: 805,
+    keywords: [
+      "sabudana",
+      "tikki",
+      "tapioca",
+      "fasting",
+      "crispy"
+    ],
+    category:'Starter',
+  },
+  {
+    id: "g43638ce-3501-4b17-9889-a3bad6fb5879",
+    image: "img/starter/samosa-chaat.jpg",
+    name: "Samosa Chaat",
+    priceCents: 885,
+    keywords: [
+      "samosa",
+      "chaat",
+      "crushed",
+      "toppings",
+      "fusion"
+    ],
+    category:'Starter',
+  },
+  {
+    id: "g43638ce-3501-4b17-9889-a3bad6fb5890",
+    image: "img/starter/samosa-recipe.jpg",
+    name: "Classic Samosa",
+    priceCents: 760,
+    keywords: [
+      "samosa",
+      "classic",
+      "fried",
+      "potato",
+      "triangular"
+    ],
+    category:'Starter',
+  },
+  {
+    id: "g43638ce-3501-4b17-9889-a3bad6fb5891",
+    image: "img/starter/sev-puri.jpg",
+    name: "Sev Puri",
+    priceCents: 695,
+    keywords: [
+      "sev puri",
+      "chaat",
+      "crispy",
+      "toppings",
+      "mumbai street food"
+    ],
+    category:'Starter',
+  },
+  {
+    id: "g43638ce-3501-4b17-9889-a3bad6fb5892",
+    image: "img/starter/spicy-guacamole.jpg",
+    name: "Spicy Guacamole",
+    priceCents: 1050,
+    keywords: [
+      "guacamole",
+      "avocado",
+      "spicy",
+      "mexican",
+      "dip"
+    ],
+    category:'Starter',
+  },
+  {
+    id: "g43638ce-3501-4b17-9889-a3bad6fb5893",
+    image: "img/starter/sweet-potato-tikki.jpg",
+    name: "Sweet Potato Tikki",
+    priceCents: 915,
+    keywords: [
+      "sweet potato",
+      "tikki",
+      "healthy",
+      "pan fried",
+      "nutritious"
+    ],
+    category:'Starter',
+  }
+]
