a71a0840a636ad100697cd39a8336131acd40868
Akshay <akshayghatge6011@gmail.com>
2025-09-04 22:05:37 -0400

    make the category item menu interactive

--- POS/scripts/pos.js
+++ POS/scripts/pos.js
@@ -1,12 +1,36 @@
-  
-  // On the click of the Add button, show Added message
+//Add the click event listener for all the add buttons
 document.querySelectorAll('.js-add-to-cart-button').forEach(button => {
     button.addEventListener('click', (e) => {
         e.preventDefault();
+        //get the Added message
         const addedMessage = button.parentElement.querySelector('.js-added-to-cart');
+        //Once the add is click show Added message
         addedMessage.style.opacity = '1';
         setTimeout(() => {
+            //display it till 2 sec and make it hide
             addedMessage.style.opacity = '0';
         }, 2000);
     });
-});``
+});
+
+//Variables to get the menu categoryItem
+const getCategories=document.querySelectorAll('.js-category-item');
+
+//Loop through each category item and add click event listener
+getCategories.forEach(categoryItem=>{
+    categoryItem.addEventListener('click', (e)=>{
+
+        const target= e.target;
+
+        //remove the selected class by looping through each categories item and remove the class name selected
+        getCategories.forEach(item=>{
+            item.classList.remove('selected');
+        })
+
+        //add the selected class to the target element
+        target.classList.add('selected');
+    
+    })
+})
+
+
