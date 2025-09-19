e2668e7d63cc8e4581114ec8504bec70a6a72f72
Akshay <akshayghatge6011@gmail.com>
2025-09-19 07:38:16 -0400

    intial commit to make order section interactive

--- POS/data/orders.js
+++ POS/data/orders.js
@@ -27,12 +27,60 @@ export function addOrders(orderItem)
     saveToStorageOrders();
 }
 
+const custNameSection=document.querySelector('.js-customer-info');
+const orderitems=document.querySelector('.js-order-items');
+const orderPaymentDetails=document.querySelector('.js-order-payment-details');
+
 document.addEventListener('DOMContentLoaded', function(){
 
-    let orderDetails='';
-    let orderGridHtml='';
+    let orderPayDetails='';
+    let newOrderItem='';
+    let customerNameSection='';
+    orders.forEach((orderItem)=>{
 
+        customerNameSection=`
+                <p><strong>Customer Name:</strong>${orderItem.customerName}</p>
+                <p><strong>Order #:</strong> ${orderItem.orderId}</p>
+                <p><strong>Order Date:</strong>${orderItem.timestamp}</p>
+            `;
+
+        orderItem.items.forEach(()=>{
+                newOrderItem=document.createElement('tr');
+                newOrderItem.innerHTML=`
+                    <td>${orderItem.items.name}</td>
+                    <td>${orderItem.items.quantity}</td>
+                    <td>${orderItem.items.npriceame}</td>
+                `;
+        })
+
+        orderPayDetails=`   
+            <tr>
+                    <td>Subtotal</td>
+                    <td>$${orderItem.subtotal}</td>
+                </tr>
+                <tr>
+                    <td>Tax (13%)</td>
+                    <td>$${orderItem.tax}</td>
+                </tr>
+                <tr>
+                    <td>Discount</td>
+                    <td>$${orderItem.discount}</td>
+                </tr>
+                <tr class="total">
+                    <td>Total</td>
+                    <td>$${orderItem.total}</td>
+            </tr>
+            `;
+    })
     
+//append the customer name section
+custNameSection.innerHTML=customerNameSection;
+console.log(newOrderItem);
+//append the order item
+orderitems.appendChild(newOrderItem);
+
+//append the payment 
+orderPaymentDetails.innerHTML=orderPaymentDetails;
 
 
 
